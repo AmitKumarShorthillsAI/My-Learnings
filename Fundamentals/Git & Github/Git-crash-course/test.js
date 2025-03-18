a4617e5b88a4f56d38e57d70d6974ad7ff5070b4
Amit kumar <Amit.kumar2@shorthills.ai>
2025-03-18 12:15:48 +0530

    commit-1 for git rebase

--- Fundamentals/Git & Github/Git-crash-course/test.js
+++ Fundamentals/Git & Github/Git-crash-course/test.js
@@ -3,3 +3,5 @@ console.log("Starting git fresh!")
 console.log("Creating a new branch -> feat/feat-a.")
 
 console.log("Creating a new branch -> feat/feat-b to merge using CLI.")
+
+console.log("Checking for Git Rebase in git.")
